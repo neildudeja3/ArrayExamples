a2cff47e389ce3305cc0a5f57ea2c1fb9aeffd55
neildudeja3 <43892691+neildudeja3@users.noreply.github.com>
2018-10-12 11:58:08 -0700

    3,4,5 TASK

--- Main.java
+++ Main.java
@@ -58,7 +58,17 @@ class Main
         /* 
          * Task 3. Add 1 to each element in ar1.  Print it out below
          */
-        
+         System.out.println("** Task 3 **");
+
+ for (int i=0 ; i<ar1.length ; i++)
+ {
+     ar1[i] = ar1[i] +1;
+ }
+
+ for (int i=0 ; i<ar1.length ; i++)
+     System.out.println("ar1[" + i + "] = "+ar1[i]);
+
+
 
         
         
@@ -69,12 +79,40 @@ class Main
          *         ar1: 1 2 3
          *         ar3: 1 2 3 0 1 2 3
          */
+               System.out.println("** test 4 **");
+        int[] ar3 = new int[ ar1.length *2 ];
+        for (int i=0 ; i< ar1.length ; i++)
+        {
+            ar3[i]=ar1[i];
+        }
         
+        for (int i=0 ; i<ar1.length ; i++)
+        {
+            ar3[i+ar1.length]=ar1[i];
+        }
+        
+        for (int i=0 ; i<ar3.length ; i++)
+            System.out.println("ar3[" + i + "] = "+ar3[i]);
         
         /*
          * Task 5.  Switch the first and last element of ar1.
          *          print out the new ar1.  Then switch them back
          */
+          System.out.println("** Task 5**");
+        // Ye olde switcheroo
+        int value;
+        value=ar1[0];
+        ar1[0]=ar1[ar1.length-1];
+        ar1[ar1.length-1]=value;
+        
+        for (int i=0 ; i<ar1.length ; i++)
+            System.out.println("ar1[" + i + "] = "+ar1[i]);
+        
+        // The Restoration of the rightful heir
+        value=ar1[0];
+        ar1[0]=ar1[ar1.length-1];
+        ar1[ar1.length-1]=value;
+        
         
         /*
          * Task 6A. Print the 2nd to (n-1)th elements of ar1
