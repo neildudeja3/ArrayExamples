968623aaa71a32c1fb0ecd27bac33fed92845f3f
neildudeja3 <43892691+neildudeja3@users.noreply.github.com>
2018-10-30 12:08:51 -0700

    factorial

---
+++ factorial.java
@@ -0,0 +1,16 @@
+public class factorial
+{
+    public static void main(String[] args)
+    {
+        int ans = factorial(13);
+        System.out.println(ans);
+    }
+        public static int factorial(int n)
+        {
+            System.out.println("DEBUG"+n);
+            if(n<=1)
+                return 1;
+                
+                return n*factorial(n-1);
+            }
+        }
